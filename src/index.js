839fc53194716e9bd9ca8c53c8ccb13aa556e69f
Lech Gudalewicz <lechgu@gmail.com>
2020-03-18 18:29:25 -0700

    background

--- src/index.js
+++ src/index.js
@@ -11,25 +11,42 @@ class MainScene extends Phaser.Scene {
 
   preload() {
     this.load.bitmapFont('font24', font24_png, font24_fnt);
+    const texture = this.textures.createCanvas(
+      'background',
+      this.opts.width,
+      this.opts.height
+    );
+    const ctx = texture.getContext();
+    ctx.fillStyle = '#282a34';
+    ctx.fillRect(0, 0, this.opts.width, this.opts.height);
+    texture.refresh();
   }
 
   create() {
-    const txt = this.add.bitmapText(
+    this.add.image(this.opts.width / 2, this.opts.height / 2, 'background');
+    this.add.bitmapText(
       this.opts.width / 2,
       this.opts.height / 2,
       'font24',
-      'Hello Pong'
+      'Hello,  Pong'
     );
-    console.log(txt.width);
   }
 
   update() {}
 }
 
 window.onload = () => {
+  const WINDOW_WIDTH = 1280;
+  const WINDOW_HEIGHT = 720;
+  const VIRTUAL_WIDTH = 432;
+  const VIRTUAL_HEIGHT = 243;
   const opts = {
-    width: 1280,
-    height: 720
+    width: WINDOW_WIDTH,
+    height: WINDOW_HEIGHT,
+    virtWidth: VIRTUAL_WIDTH,
+    virtHeight: VIRTUAL_HEIGHT,
+    scaleX: WINDOW_WIDTH / VIRTUAL_WIDTH,
+    scaleY: WINDOW_HEIGHT / VIRTUAL_HEIGHT
   };
 
   const mainScene = new MainScene(opts);
@@ -38,7 +55,6 @@ window.onload = () => {
     width: opts.width,
     height: opts.height,
     backgroundColor: '#000000',
-    type: Phaser.CANVAS,
     scene: [mainScene]
   };
   const game = new Phaser.Game(config);
