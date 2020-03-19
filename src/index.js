948e8c9f95083dfbe8de326bde2d61a8f841633c
Lech Gudalewicz <lechgu@gmail.com>
2020-03-19 12:52:51 -0700

    move paddles

--- src/index.js
+++ src/index.js
@@ -13,7 +13,16 @@ class Paddle {
   }
 
   create() {
-    this.game.add.image(this.x, this.y, 'paddle');
+    this.sprite = this.game.add.image(this.x, this.y, 'paddle');
+  }
+
+  incrementY(dy) {
+    this.y += dy;
+  }
+
+  update(dt) {
+    this.sprite.x = this.x;
+    this.sprite.y = this.y;
   }
 }
 
@@ -25,7 +34,41 @@ class Ball {
   }
 
   create() {
-    this.game.add.image(this.x, this.y, 'ball');
+    this.sprite = this.game.add.image(this.x, this.y, 'ball');
+  }
+}
+
+class Logo {
+  constructor(game, x, y) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+  }
+
+  create() {
+    this.text = this.game.add.bitmapText(
+      this.x,
+      this.y,
+      'font24',
+      'Hello Pong'
+    );
+  }
+}
+
+class Score {
+  constructor(game, x, y) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.score = 0;
+  }
+
+  create() {
+    this.text = this.game.add.bitmapText(this.x, this.y, 'font96', '');
+  }
+
+  update(dt) {
+    this.text.text = `${this.score}`;
   }
 }
 
@@ -33,24 +76,50 @@ class MainScene extends Phaser.Scene {
   constructor(opts) {
     super('MainScene');
     this.opts = opts;
-  }
-
-  update(dx) {}
-
-  createSolidSprite(name, width, height, color) {
-    const texture = this.textures.createCanvas(name, width, height);
-    const ctx = texture.getContext();
-    ctx.fillStyle = color;
-    ctx.fillRect(0, 0, width, height);
-    texture.refresh();
-
     this.paddleA = new Paddle(this, 100, 100);
     this.paddleB = new Paddle(
       this,
       this.opts.width - 100,
       this.opts.height - 100
     );
+    this.scoreA = new Score(this, 200, 200);
+    this.scoreB = new Score(this, this.opts.width - 200, 200);
     this.ball = new Ball(this, this.opts.width / 2, this.opts.height / 2);
+    this.logo = new Logo(this, this.opts.width / 2, 100);
+  }
+
+  create() {
+    this.add.image(this.opts.width / 2, this.opts.height / 2, 'background');
+    this.paddleA.create();
+    this.paddleB.create();
+    this.ball.create();
+    this.scoreA.create();
+    this.scoreB.create();
+    this.logo.create();
+    this.keyW = this.input.keyboard.addKey('w');
+    this.keyS = this.input.keyboard.addKey('s');
+    this.keyUp = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+    this.keyDown = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.DOWN
+    );
+  }
+  update(tm, dt) {
+    const paddleSpeed = 0.3;
+    this.paddleA.update(dt);
+    this.paddleB.update(dt);
+    this.scoreA.update(dt);
+    this.scoreB.update(dt);
+
+    if (this.keyW.isDown) {
+      this.paddleA.incrementY(-paddleSpeed * dt);
+    } else if (this.keyS.isDown) {
+      this.paddleA.incrementY(paddleSpeed * dt);
+    }
+    if (this.keyUp.isDown) {
+      this.paddleB.incrementY(-paddleSpeed * dt);
+    } else if (this.keyDown.isDown) {
+      this.paddleB.incrementY(paddleSpeed * dt);
+    }
   }
 
   preload() {
@@ -66,18 +135,13 @@ class MainScene extends Phaser.Scene {
     this.createSolidSprite('ball', 12, 12, '#ffffff');
   }
 
-  create() {
-    this.add.image(this.opts.width / 2, this.opts.height / 2, 'background');
-    this.paddleA.create();
-    this.paddleB.create();
-    this.ball.create();
-    //this.add.image(100, 100, 'paddle');
-    // this.add.image(this.opts.width - 100, this.opts.height - 100, 'paddle');
-    //this.add.image(this.opts.width / 2, this.opts.height / 2, 'ball');
-
-    this.add.bitmapText(this.opts.width / 2, 100, 'font24', 'Hello Pong!');
+  createSolidSprite(name, width, height, color) {
+    const texture = this.textures.createCanvas(name, width, height);
+    const ctx = texture.getContext();
+    ctx.fillStyle = color;
+    ctx.fillRect(0, 0, width, height);
+    texture.refresh();
   }
-  update() {}
 }
 
 window.onload = () => {
